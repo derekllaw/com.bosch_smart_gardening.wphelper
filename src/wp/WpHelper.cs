bd2a1c917ae89c0edf1da432dc12162f9c56e893
derekllaw <derek@ballandia.co.uk>
2015-06-04 16:47:27 +0100

    Real Code Added

--- src/wp/WpHelper.cs
+++ src/wp/WpHelper.cs
@@ -2,21 +2,23 @@ using System;
 
 namespace WPCordovaClassLib.Cordova.Commands
 {
-    public class WpHelper : BaseCommand
+  public class WpHelper : BaseCommand
+  {
+    public void ToUpper(string options)
     {
-        public void ToUpper(string options)
-        {
-            string upperCase = JSON.JsonHelper.Deserialize<string[]>(options)[0].ToUpper();
-            PluginResult result;
-            if (upperCase != "")
-            {
-                result = new PluginResult(PluginResult.Status.OK, upperCase);
-            } else
-            {
-                result = new PluginResult(PluginResult.Status.ERROR, upperCase);
-            }
+      string upperCase = JSON.JsonHelper.Deserialize<string[]>(options)[0].ToUpper();
+      PluginResult result;
+      if (upperCase != "")
+      {
+        result = new PluginResult(PluginResult.Status.OK, upperCase);
+      } else
+      {
+        result = new PluginResult(PluginResult.Status.ERROR, upperCase);
+      }
 
-            DispatchCommandResult(result);
-        }
+      Windows.Graphics.Display.DisplayInformation.AutoRotationPreferences = Windows.Graphics.Display.DisplayOrientations.Portrait;
+
+      DispatchCommandResult(result);
     }
+  }
 }
